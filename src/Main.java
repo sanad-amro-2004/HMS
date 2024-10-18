bb41257130ae89c7195cc82d6053b84c907f6473
sanad amro <sanadamro@sanads-MacBook-Air.local>
2024-10-18 08:54:48 +0300

    this is my first gui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -7,6 +10,17 @@ public class Main {
         System.out.printf("Hello    and welcome!");
 
         for (int i = 1; i <= 5; i++) {
+            JFrame myFrame=new JFrame();
+            myFrame.setSize(700,700);
+            myFrame.setVisible(true);
+            myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            myFrame.setTitle("Hi Nigger");
+
+            JLabel lable =new JLabel();
+            lable.setForeground(new Color(0,50,250));
+            myFrame.add(lable);
+            lable.setText("hellow mother fucker");
+
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
